6ab8f6f2ae9731e9db97f314733436da676bfad2
rodrigodevts <rodrigo.sousa.devjs@gmail.com>
2022-12-20 11:25:12 -0300

    chore: add lin-staged and husky

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    "prettier",
+    "plugin:prettier/recommended"
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    'prettier',
+  ],
+  rules: {
+    "prettier/prettier": "error",
+  }
+}
