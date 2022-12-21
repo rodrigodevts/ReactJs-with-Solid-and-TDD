abcdc88421f9f084ef05e64807bea96d83654e28
rodrigodevts <rodrigo.sousa.devjs@gmail.com>
2022-12-20 23:38:23 -0300

    refactor: changed IAuthenticate for Authenticate file name

--- src/domain/test/mock-authentication.ts
+++
@@ -1,9 +0,0 @@
-import { faker } from "@faker-js/faker";
-import { IAuthenticationParams } from "@/domain/useCases/IAuthentication";
-
-const mockAuthentication = (): IAuthenticationParams => ({
-  email: faker.internet.email(),
-  password: faker.internet.password(),
-});
-
-export { mockAuthentication };
