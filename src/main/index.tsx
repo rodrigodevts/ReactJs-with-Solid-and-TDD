f9ea0c8673a8977e0db2be62467cc9297e11a16d
rodrigodevts <rodrigo.sousa.devjs@gmail.com>
2023-01-01 22:20:58 -0300

    fix: move global styles to index in main file

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Router } from '@/presentation/components';
+import '@/presentation/styles/global.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/presentation/components/router/index.tsx
+++ src/presentation/components/router/index.tsx
@@ -1,8 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Login } from '@/presentation/pages';
 
-import '@/presentation/styles/global.scss';
-
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
