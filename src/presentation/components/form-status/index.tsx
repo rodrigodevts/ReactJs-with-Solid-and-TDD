74857e2e687ecc580400d38d305a4af949a7cf45
rodrigodevts <rodrigo.sousa.devjs@gmail.com>
2023-01-01 22:41:57 -0300

    feat: ensure Login will not load spinner and error on start

--- src/presentation/components/form-status/index.tsx
+++ src/presentation/components/form-status/index.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { FormContext } from '@/presentation/contexts/formContext';
 import './form-status-styles.scss';
 
 const FormStatus: React.FC = () => {
+  const { errorMessage } = useContext(FormContext);
   return (
-    <div className="errorWrap">
-      <span className="error">Erro</span>
+    <div data-testid="error-wrap" className="errorWrap">
+      {errorMessage && <span className="error">{errorMessage}</span>}
     </div>
   );
 };
--- src/presentation/pages/Login/index.tsx
+++ src/presentation/pages/Login/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
   Footer,
   FormStatus,
@@ -7,22 +8,42 @@ import {
 } from '@/presentation/components';
 
 import './login-styles.scss';
+import { FormContext } from '@/presentation/contexts/formContext';
+
+type StateProps = {
+  isLoading: boolean;
+  errorMessage: string;
+};
 
 function Login() {
+  const [state] = useState<StateProps>({
+    isLoading: false,
+    errorMessage: '',
+  });
+
   return (
     <div className="login">
       <LoginHeader />
-      <form action="" className="form">
-        <h2>Login</h2>
-        <Input type="email" name="email" placeholder="Digite seu e-mail" />
-        <Input type="password" name="password" placeholder="Digite sua senha" />
-        <button className="buttonSubmit" type="submit">
-          {/* Entrar */}
-          <Spinner />
-        </button>
-        <FormStatus />
-        <span className="createCountLink">Criar conta</span>
-      </form>
+      <FormContext.Provider value={state}>
+        <form action="" className="form">
+          <h2>Login</h2>
+          <Input type="email" name="email" placeholder="Digite seu e-mail" />
+          <Input
+            type="password"
+            name="password"
+            placeholder="Digite sua senha"
+          />
+          <button
+            data-testid="spinner-loading"
+            className="buttonSubmit"
+            type="submit"
+          >
+            {state.isLoading ? <Spinner /> : 'Entrar'}
+          </button>
+          <FormStatus />
+          <span className="createCountLink">Criar conta</span>
+        </form>
+      </FormContext.Provider>
       <Footer />
     </div>
   );
