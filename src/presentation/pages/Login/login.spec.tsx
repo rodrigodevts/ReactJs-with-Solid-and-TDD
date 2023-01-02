d58a516dd1711f1a8ff701a3c6a905bf0d9ef687
rodrigodevts <rodrigo.sousa.devjs@gmail.com>
2023-01-01 22:56:14 -0300

    test: ensure Login status with submit button disabled

--- src/presentation/pages/Login/login.spec.tsx
+++ src/presentation/pages/Login/login.spec.tsx
@@ -2,14 +2,15 @@ import { render } from '@testing-library/react';
 import Login from '.';
 
 describe('Login component', () => {
-  test('Should not render error on start', () => {
+  test('Should start with initial state', () => {
     const { getByTestId } = render(<Login />);
+
     const errorWrap = getByTestId('error-wrap');
     expect(errorWrap.childElementCount).toBe(0);
-  });
-  test('Should not render spinner on start', () => {
-    const { getByTestId } = render(<Login />);
-    const spinnerLoading = getByTestId('spinner-loading');
-    expect(spinnerLoading.childElementCount).toBe(0);
+
+    const buttonSubmit = getByTestId('button-submit') as HTMLButtonElement;
+    expect(buttonSubmit.childElementCount).toBe(0);
+
+    expect(buttonSubmit.disabled).toBe(true);
   });
 });
