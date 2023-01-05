7eacfcbd3a9f518df75d5e5d64b0e77830591d48
rodrigodevts <rodrigo.sousa.devjs@gmail.com>
2023-01-05 08:41:02 -0300

    feat: ensure Login calls authentication with correct values

--- src/presentation/components/router/index.tsx
+++ src/presentation/components/router/index.tsx
@@ -1,13 +1,35 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Login } from '@/presentation/pages';
 import { ValidationStub } from '@/presentation/test/mock-validation';
+import { Authentication, AuthenticationParams } from '@/domain/useCases';
+import { mockAccountModel } from '@/domain/test';
+import { AccountModel } from '@/domain/models';
+
+class AuthenticationSpy implements Authentication {
+  account = mockAccountModel();
+  params: AuthenticationParams;
+
+  async auth(params: AuthenticationParams): Promise<AccountModel> {
+    this.params = params;
+    return await Promise.resolve(this.account);
+  }
+}
 
 const Router: React.FC = () => {
   const validationStub = new ValidationStub();
+  const authentication = new AuthenticationSpy();
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login validation={validationStub} />} />
+        <Route
+          path="/login"
+          element={
+            <Login
+              validation={validationStub}
+              authentication={authentication}
+            />
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
--- src/presentation/pages/Login/index.tsx
+++ src/presentation/pages/Login/index.tsx
@@ -11,12 +11,14 @@ import { FormContext } from '@/presentation/contexts/formContext';
 
 import './login-styles.scss';
 import { Validation } from '@/presentation/protocols/validation';
+import { Authentication } from '@/domain/useCases';
 
 type LoginProps = {
   validation: Validation;
+  authentication: Authentication;
 };
 
-function Login({ validation }: LoginProps) {
+function Login({ validation, authentication }: LoginProps) {
   const [state, setState] = useState({
     isLoading: false,
     email: '',
@@ -34,10 +36,16 @@ function Login({ validation }: LoginProps) {
     });
   }, [state.email, state.password]);
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+  const handleSubmit = async (
+    event: React.FormEvent<HTMLFormElement>
+  ): Promise<void> => {
     event.preventDefault();
 
     setState({ ...state, isLoading: true });
+    await authentication.auth({
+      email: state.email,
+      password: state.password,
+    });
   };
 
   return (
