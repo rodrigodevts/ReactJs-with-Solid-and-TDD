868430241c6eece644244546799804796267b66d
rodrigodevts <rodrigo.sousa.devjs@gmail.com>
2023-01-05 19:58:58 -0300

    test: prevent Login to call submit more than once

--- src/presentation/pages/Login/login.spec.tsx
+++ src/presentation/pages/Login/login.spec.tsx
@@ -147,4 +147,11 @@ describe('Login component', () => {
       password,
     });
   });
+
+  test('Should call Authentication only once', () => {
+    const { sut, authenticationSpy } = makeSut();
+    simulateValidSubmit(sut);
+    simulateValidSubmit(sut);
+    expect(authenticationSpy.callsCount).toBe(1);
+  });
 });
