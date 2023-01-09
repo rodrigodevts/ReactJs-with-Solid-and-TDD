08d0db6d26c5c92d626cb212d09c9eca3a73894f
rodrigodevts <rodrigo.sousa.devjs@gmail.com>
2023-01-08 22:33:54 -0300

    refactor: move SUT creation to a factory method

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -2,15 +2,18 @@ import { RequiredFieldError } from '@/validation/errors/required-field-error';
 import { faker } from '@faker-js/faker';
 import { RequiredFieldValidation } from './required-field-validation';
 
+const makeSut = (): RequiredFieldValidation =>
+  new RequiredFieldValidation(faker.database.column());
+
 describe('RequiredFieldValidation', () => {
   test('Should return error if field is empty', () => {
-    const sut = new RequiredFieldValidation('email');
+    const sut = makeSut();
     const error = sut.validate('');
     expect(error).toEqual(new RequiredFieldError());
   });
 
   test('Should return falsy if field is not empty', () => {
-    const sut = new RequiredFieldValidation('email');
+    const sut = makeSut();
     const error = sut.validate(faker.random.word());
     expect(error).toBeFalsy();
   });
