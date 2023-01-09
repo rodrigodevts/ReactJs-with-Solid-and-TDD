2ca41b95a3da7574351515aef9895cbe0020cc07
rodrigodevts <rodrigo.sousa.devjs@gmail.com>
2023-01-09 15:47:42 -0300

    feat: ensure EmailValidation returns falsy if email is valid

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-escape */
 import { InvalidFieldError } from '@/validation/errors/invalid-field-error';
 import { FieldValidation } from '@/validation/protocols/field-validation';
 
@@ -5,7 +6,9 @@ class EmailValidation implements FieldValidation {
   constructor(readonly field: string) {}
 
   validate(value: string): Error {
-    return new InvalidFieldError();
+    const emailRegex =
+      /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    return emailRegex.test(value) ? null : new InvalidFieldError();
   }
 }
 
