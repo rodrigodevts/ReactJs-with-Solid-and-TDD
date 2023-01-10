3c81f98759e394cef398dc667e09179b987fc56b
rodrigodevts <rodrigo.sousa.devjs@gmail.com>
2023-01-10 00:13:11 -0300

    test: ensure ValidationBuilder returns a list of validations

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -1,21 +1,44 @@
 import { RequiredFieldValidation } from '@/validation/validators/required-field/required-field-validation';
+import { faker } from '@faker-js/faker';
 import { EmailValidation } from '../email/email-validation';
 import { MinLengthValidation } from '../min-length/min-length-validation';
 import { ValidationBuilder as sut } from './validation-builder';
 
 describe('ValidationBuilder', () => {
   test('Should return RequiredFieldValidation', () => {
-    const validations = sut.field('any_field').required().build();
-    expect(validations).toEqual([new RequiredFieldValidation('any_field')]);
+    const field = faker.database.column();
+    const validations = sut.field(field).required().build();
+    expect(validations).toEqual([new RequiredFieldValidation(field)]);
   });
 
   test('Should return EmailValidation', () => {
-    const validations = sut.field('any_field').email().build();
-    expect(validations).toEqual([new EmailValidation('any_field')]);
+    const field = faker.database.column();
+    const validations = sut.field(field).email().build();
+    expect(validations).toEqual([new EmailValidation(field)]);
   });
 
   test('Should return MinLengthValidation', () => {
-    const validations = sut.field('any_field').min(5).build();
-    expect(validations).toEqual([new MinLengthValidation('any_field', 5)]);
+    const field = faker.database.column();
+    const length = faker.random.numeric();
+    const validations = sut.field(field).min(Number(length)).build();
+    expect(validations).toEqual([
+      new MinLengthValidation(field, Number(length)),
+    ]);
+  });
+
+  test('Should return a list of validations', () => {
+    const field = faker.database.column();
+    const length = faker.random.numeric(5);
+    const validations = sut
+      .field(field)
+      .required()
+      .min(Number(length))
+      .email()
+      .build();
+    expect(validations).toEqual([
+      new RequiredFieldValidation(field),
+      new MinLengthValidation(field, Number(length)),
+      new EmailValidation(field),
+    ]);
   });
 });
