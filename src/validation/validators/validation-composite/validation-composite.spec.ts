944af283232209d51c5df2ca7b07607b76c58de0
rodrigodevts <rodrigo.sousa.devjs@gmail.com>
2023-01-09 23:44:21 -0300

    test: ensure ValidationComposite returns falsy if all validation are valid

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -1,3 +1,4 @@
+import { faker } from '@faker-js/faker';
 import { FieldValidationSpy } from '../test/mock-field-validation';
 import { ValidationComposite } from './validation-composite';
 
@@ -6,10 +7,10 @@ type SutTypes = {
   fieldValidationsSpy: FieldValidationSpy[];
 };
 
-const makeSut = (): SutTypes => {
+const makeSut = (fieldName: string): SutTypes => {
   const fieldValidationsSpy = [
-    new FieldValidationSpy('any_field'),
-    new FieldValidationSpy('any_field'),
+    new FieldValidationSpy(fieldName),
+    new FieldValidationSpy(fieldName),
   ];
 
   const sut = new ValidationComposite(fieldValidationsSpy);
@@ -22,10 +23,19 @@ const makeSut = (): SutTypes => {
 
 describe('ValidationComposite', () => {
   test('Should return error if any validation fails', () => {
-    const { sut, fieldValidationsSpy } = makeSut();
-    fieldValidationsSpy[0].error = new Error('first_error_message');
-    fieldValidationsSpy[1].error = new Error('second_error_message');
-    const error = sut.validate('any_field', 'any_value');
-    expect(error).toBe('first_error_message');
+    const fieldName = faker.database.column();
+    const { sut, fieldValidationsSpy } = makeSut(fieldName);
+    const errorMessage = faker.random.words();
+    fieldValidationsSpy[0].error = new Error(errorMessage);
+    fieldValidationsSpy[1].error = new Error(faker.random.words());
+    const error = sut.validate(fieldName, faker.random.word());
+    expect(error).toBe(errorMessage);
+  });
+
+  test('Should return falsy if all validation are valid', () => {
+    const fieldName = faker.database.column();
+    const { sut } = makeSut(fieldName);
+    const error = sut.validate(fieldName, faker.random.word());
+    expect(error).toBeFalsy();
   });
 });
