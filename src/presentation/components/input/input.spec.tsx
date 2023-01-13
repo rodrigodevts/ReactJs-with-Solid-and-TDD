86b724c9bb6f43a670b2436eafbec2a61f1d7b69
rodrigodevts <rodrigo.sousa.devjs@gmail.com>
2023-01-12 22:22:22 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/components/input/input.spec.tsx
+++ src/presentation/components/input/input.spec.tsx
@@ -1,23 +1,27 @@
-import { render } from '@testing-library/react';
+import { render, RenderResult } from '@testing-library/react';
 import Input from './';
 import { FormContext } from '@/presentation/contexts/formContext';
 
+const makeSut = (): RenderResult => {
+  const state = {
+    isLoading: false,
+    email: '',
+    password: '',
+    emailError: '',
+    passwordError: '',
+    mainError: '',
+  };
+  return render(
+    <FormContext.Provider value={{ state, setState: () => {} }}>
+      <Input name="field" />
+    </FormContext.Provider>
+  );
+};
+
 describe('Input Component', () => {
   test('Should begin with readOnly', () => {
-    const state = {
-      isLoading: false,
-      email: '',
-      password: '',
-      emailError: '',
-      passwordError: '',
-      mainError: '',
-    };
-    const { getByTestId } = render(
-      <FormContext.Provider value={{ state, setState: () => {} }}>
-        <Input name="field" />
-      </FormContext.Provider>
-    );
-    const input = getByTestId('field') as HTMLInputElement;
+    const sut = makeSut();
+    const input = sut.getByTestId('field') as HTMLInputElement;
     expect(input.readOnly).toBe(true);
   });
 });
