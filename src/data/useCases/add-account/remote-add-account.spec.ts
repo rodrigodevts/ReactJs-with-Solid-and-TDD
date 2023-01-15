a3710a957f34b81bb38792ca5e0d230b793a2719
rodrigodevts <rodrigo.sousa.devjs@gmail.com>
2023-01-15 10:52:35 -0300

    refactor: move SUT creation to a factory method

--- src/data/useCases/add-account/remote-add-account.spec.ts
+++ src/data/useCases/add-account/remote-add-account.spec.ts
@@ -5,14 +5,29 @@ import { AddAccountParams } from '@/domain/useCases/add-account';
 import { faker } from '@faker-js/faker';
 import { RemoteAddAccount } from './remote-add-account';
 
+type SutTypes = {
+  sut: RemoteAddAccount;
+  httpPostClientSpy: HttpPostClientSpy<AddAccountParams, AccountModel>;
+};
+
+const makeSut = (url = faker.internet.url()): SutTypes => {
+  const httpPostClientSpy = new HttpPostClientSpy<
+    AddAccountParams,
+    AccountModel
+  >();
+  const sut = new RemoteAddAccount(url, httpPostClientSpy);
+
+  return {
+    sut,
+    httpPostClientSpy,
+  };
+};
+
 describe('RemoteAddAccount', () => {
   test('Should call HttpPostClient with correct URL', async () => {
-    const httpPostClientSpy = new HttpPostClientSpy<
-      AddAccountParams,
-      AccountModel
-    >();
     const url = faker.internet.url();
-    const sut = new RemoteAddAccount(url, httpPostClientSpy);
+    const { sut, httpPostClientSpy } = makeSut(url);
     await sut.add(mockAddAccountParams());
+    expect(httpPostClientSpy.url).toBe(url);
   });
 });
