68f1e9de11783bc20a17b3e2c88a1cb140d1fe44
rodrigodevts <rodrigo.sousa.devjs@gmail.com>
2023-01-16 21:10:39 -0300

    feat: ensure CompareFieldValidation returns falsy if validation succeeds

--- src/validation/validators/compare-fields/compare-fields-validation.ts
+++ src/validation/validators/compare-fields/compare-fields-validation.ts
@@ -5,7 +5,7 @@ class CompareFieldsValidation implements FieldValidation {
   constructor(readonly field: string, readonly valueToCompare: string) {}
 
   validate(value: string): Error {
-    return new InvalidFieldError();
+    return value !== this.valueToCompare ? new InvalidFieldError() : null;
   }
 }
 
