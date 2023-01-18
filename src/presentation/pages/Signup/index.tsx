481a96e15f21e8b7272b40bb9e801b1838b8e6ac
rodrigodevts <rodrigo.sousa.devjs@gmail.com>
2023-01-17 22:12:13 -0300

    feat: ensure SignUp change passwordConfirmation status to error if validation fails

--- src/presentation/pages/Signup/index.tsx
+++ src/presentation/pages/Signup/index.tsx
@@ -20,10 +20,11 @@ function Signup({ validation }: SignUpProps) {
     name: '',
     nameError: '',
     email: '',
-    emailError: 'Campo obrigatório',
+    emailError: '',
     password: '',
-    passwordError: 'Campo obrigatório',
-    passwordConfirmationError: 'Campo obrigatório',
+    passwordError: '',
+    passwordConfirmation: '',
+    passwordConfirmationError: '',
     mainError: '',
   });
 
@@ -33,8 +34,12 @@ function Signup({ validation }: SignUpProps) {
       nameError: validation.validate('name', state.name),
       emailError: validation.validate('email', state.email),
       passwordError: validation.validate('password', state.password),
+      passwordConfirmationError: validation.validate(
+        'passwordConfirmation',
+        state.passwordConfirmation
+      ),
     });
-  }, [state.name, state.email, state.password]);
+  }, [state.name, state.email, state.password, state.passwordConfirmation]);
 
   return (
     <div className="signup">
