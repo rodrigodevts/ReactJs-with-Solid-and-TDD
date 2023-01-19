755382f129dc9a5a8bc022dd0326477a849d6304
rodrigodevts <rodrigo.sousa.devjs@gmail.com>
2023-01-19 10:01:13 -0300

    test: add SignUpValidationFactory

---
+++ src/main/factories/pages/signup/signup-validation-factory.spec.ts
@@ -0,0 +1,17 @@
+import { ValidationComposite } from '@/validation/validators/validation-composite/validation-composite';
+import { makeSignUpValidation } from './signup-validation-factory';
+import { ValidationBuilder as Builder } from '@/validation/validators/builder/validation-builder';
+
+describe('SignUpValidationFactory', () => {
+  test('Should make ValidationComposite with correct validations', () => {
+    const composite = makeSignUpValidation();
+    expect(composite).toEqual(
+      ValidationComposite.build([
+        ...Builder.field('name').required().build(),
+        ...Builder.field('email').required().email().build(),
+        ...Builder.field('password').required().min(5).build(),
+        ...Builder.field('passwordConfirmation').required().min(5).build(),
+      ])
+    );
+  });
+});
