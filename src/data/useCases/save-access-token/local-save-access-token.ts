09889f14ac4d1f5ecfee7f97410b14f470e56b2a
rodrigodevts <rodrigo.sousa.devjs@gmail.com>
2023-01-21 20:02:20 -0300

    feat: ensure LocalSaveAccessToken throws if no accessToken is provided

--- src/data/useCases/save-access-token/local-save-access-token.ts
+++ src/data/useCases/save-access-token/local-save-access-token.ts
@@ -1,10 +1,14 @@
 import { SetStorage } from '@/data/protocols/cache/set-storage';
+import { UnexpectedError } from '@/domain/errors/unexpected-error';
 import { SaveAccessToken } from '@/domain/useCases/save-access-token';
 
 class LocalSaveAccessToken implements SaveAccessToken {
   constructor(private readonly setStorage: SetStorage) {}
 
   async save(accessToken: string): Promise<void> {
+    if (!accessToken) {
+      throw new UnexpectedError();
+    }
     await this.setStorage.set('react-solid@accessToken', accessToken);
   }
 }
